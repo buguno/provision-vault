f0fbdaa6a9ed20a76f9ec639d02f972989dba7b8
buguno <brunnodesouzabezerra@gmail.com>
2024-02-11 20:07:37 -0300

    test: implementing client fixture and test return new user created

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,24 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from app.app import app
-
-
-def test_should_return_200_and_hello_prepper_message():
-    client = TestClient(app)
-
+def test_should_return_200_and_hello_prepper_message(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, prepper!'}
+
+
+def test_should_return_new_user_created_and_status_code_201(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'reece',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'reece',
+    }
