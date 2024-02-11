47c991703aca34e59625d4ddf87814599af29ea5
buguno <brunnodesouzabezerra@gmail.com>
2024-02-11 19:59:05 -0300

    feat: creating provisional create user router

--- app/app.py
+++ app/app.py
@@ -2,11 +2,23 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from app.schemas import Message
+from app.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK.value, response_model=Message)
 def read_root():
     return {'message': 'Hello, prepper!'}
+
+
+@app.post(
+    '/users/', status_code=HTTPStatus.CREATED.value, response_model=UserPublic
+)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+
+    return user_with_id
