4c0b3ffeee7b35a6c054f0a135156e8ea8234c3b
buguno <brunnodesouzabezerra@gmail.com>
2024-02-11 19:57:40 -0300

    feat: creating class user base models

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,18 @@ from pydantic import BaseModel
 
 class Message(BaseModel):
     message: str
+
+
+class UserSchema(BaseModel):
+    username: str
+    password: str
+
+
+class UserPublic(BaseModel):
+    id: int
+    username: str
+    password: str
+
+
+class UserDB(UserSchema):
+    id: int
